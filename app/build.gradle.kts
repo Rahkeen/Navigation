155de893696c3d985683b8fa4e3d7a8bb61cb4bc
Rikin Marfatia <rikinm10@gmail.com>
2024-05-15 16:13:38 -0700

    Added some a Bottome Nav Bar + Hooked it up to Navigation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
   alias(libs.plugins.android.application)
   alias(libs.plugins.jetbrains.kotlin.android)
+  alias(libs.plugins.kotlinx.serialization)
 }
 
 android {
@@ -51,16 +52,22 @@ dependencies {
   implementation(libs.androidx.core.ktx)
   implementation(libs.androidx.lifecycle.runtime.ktx)
   implementation(libs.androidx.activity.compose)
+  implementation(libs.androidx.navigation.compose)
+
   implementation(platform(libs.androidx.compose.bom))
   implementation(libs.androidx.ui)
   implementation(libs.androidx.ui.graphics)
   implementation(libs.androidx.ui.tooling.preview)
   implementation(libs.androidx.material3)
+
+  implementation(libs.kotlinx.serialization)
+
   testImplementation(libs.junit)
   androidTestImplementation(libs.androidx.junit)
   androidTestImplementation(libs.androidx.espresso.core)
   androidTestImplementation(platform(libs.androidx.compose.bom))
   androidTestImplementation(libs.androidx.ui.test.junit4)
+
   debugImplementation(libs.androidx.ui.tooling)
   debugImplementation(libs.androidx.ui.test.manifest)
 }
--- app/src/main/java/dev/supergooey/navigation/MainActivity.kt
+++ app/src/main/java/dev/supergooey/navigation/MainActivity.kt
@@ -4,46 +4,129 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.core.spring
+import androidx.compose.animation.scaleOut
+import androidx.compose.animation.slideIn
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeContent
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.AccountCircle
+import androidx.compose.material.icons.rounded.DateRange
+import androidx.compose.material.icons.rounded.Home
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntOffset
+import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import dev.supergooey.navigation.Destination.One
+import dev.supergooey.navigation.Destination.Three
+import dev.supergooey.navigation.Destination.Two
 import dev.supergooey.navigation.ui.theme.NavigationTheme
+import kotlinx.serialization.Serializable
 
 class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     enableEdgeToEdge()
     setContent {
-      NavigationTheme {
-        Scaffold(
-          modifier = Modifier.fillMaxSize()
-        ) { innerPadding ->
-          Greeting(
-            name = "Android",
-            modifier = Modifier.padding(innerPadding)
-          )
+      App()
+    }
+  }
+}
+
+data class Item(
+  val label: String,
+  val icon: ImageVector,
+  val route: Destination,
+)
+
+sealed interface Destination {
+  @Serializable
+  data object One : Destination
+  @Serializable
+  data object Two : Destination
+  @Serializable
+  data object Three : Destination
+}
+
+@Composable
+fun App() {
+  val items = remember {
+    mutableStateListOf(
+      Item("One", Icons.Rounded.Home, One),
+      Item("Two", Icons.Rounded.AccountCircle, Two),
+      Item("Three", Icons.Rounded.DateRange, Three),
+    )
+  }
+  NavigationTheme {
+    val navController = rememberNavController()
+    Scaffold(
+      modifier = Modifier.fillMaxSize(),
+      contentWindowInsets = WindowInsets.safeContent,
+      bottomBar = {
+        NavigationBar {
+          items.forEach { item ->
+            NavigationBarItem(
+              selected = true,
+              label = { Text(item.label) },
+              onClick = { navController.navigate(item.route) },
+              icon = {
+                Icon(
+                  imageVector = item.icon,
+                  contentDescription = item.label
+                )
+              }
+            )
+          }
+        }
+      }
+    ) { innerPadding ->
+      NavHost(
+        modifier = Modifier.padding(innerPadding),
+        navController = navController,
+        enterTransition = { slideIn(initialOffset = { IntOffset(x = it.width, y = 0) }, animationSpec = spring()) },
+        exitTransition = { scaleOut(animationSpec = spring()) },
+        startDestination = One
+      ) {
+        composable<One> {
+          Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+            Text("One")
+          }
+        }
+        composable<Two> {
+          Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+            Text("Two")
+          }
+        }
+        composable<Three> {
+          Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+            Text("Three")
+          }
         }
       }
     }
   }
 }
 
+@Preview
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-  Text(
-    text = "Hello $name!",
-    modifier = modifier
-  )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-  NavigationTheme {
-    Greeting("Android")
-  }
+private fun AppPreview() {
+  App()
 }
